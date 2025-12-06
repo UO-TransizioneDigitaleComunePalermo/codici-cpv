9ce1a79e1655bc12671ad2c7772dbfee340afbfc
UO-TransizioneDigitaleComunePalermo <42996217+UO-TransizioneDigitaleComunePalermo@users.noreply.github.com>
2025-12-06 13:01:29 +0100

    Update locale.js

--- js/dist/locales/it/locale.js
+++ js/dist/locales/it/locale.js
@@ -66,8 +66,8 @@ Exhibit.Localization.importLocale("it", {
     "%import.failedAccessHttpStatus": " (HTTP %1$d)",
     "%import.missingFragment": "Failed to locate fragment in %1$s",
 
-    "%database.itemType.label": "codice PDC",
-    "%database.itemType.pluralLabel": "codici PDC",
+    "%database.itemType.label": "codice",
+    "%database.itemType.pluralLabel": "codici",
     "%database.labelProperty.label": "label",
     "%database.labelProperty.pluralLabel": "labels",
     "%database.labelProperty.reverseLabel": "label of",
